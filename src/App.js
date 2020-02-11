edafe71a8f5f0d9e118dcd9405522535480212b0
hakan-kemal <h.k.cuhadar@gmail.com>
2020-02-11 22:36:01 +0100

    Style Homepage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from "react";
-import "./App.css";
-import { Route } from "react-router-dom";
-import AdsListContainer from "./components/AdsListContainer";
-import AdDetailsContainer from "./components/AdDetailsContainer";
-import CreateAdContainer from "./components/CreateAdContainer";
+import React from 'react';
+import { Route } from 'react-router-dom';
+import AdsListContainer from './components/AdsListContainer';
+import AdDetailsContainer from './components/AdDetailsContainer';
+import CreateAdContainer from './components/CreateAdContainer';
+import Homepage from './components/Homepage';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img
-          src="https://cdn.iconscout.com/icon/premium/png-256-thumb/marketplace-42-672171.png"
-          className="App-logo"
-          alt="logo"
-        />
-        <p>FullStack Labs - Marketplace Application</p>
-        <Route exact path="/" component={CreateAdContainer} />
-        <Route exact path="/" component={AdsListContainer} />
-        <Route path="/advertisements/:id" component={AdDetailsContainer} />
-      </header>
+    <div>
+      <Route path='/' exact component={Homepage} />
+      <Route path='/advertisements' component={AdsListContainer} />
+      <Route path='/advertisements' component={CreateAdContainer} />
+      <Route path='/advertisements/:id' component={AdDetailsContainer} />
     </div>
   );
-}
+};
 
 export default App;
